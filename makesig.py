315d027abd3eef39c7e0272cb3aded62fc60494a
John Mascagni <johnmascagni@gmail.com>
2020-08-17 20:45:37 -0500

    Fix makesig

--- makesig.py
+++ makesig.py
@@ -1,36 +1,24 @@
-import idautils
-import ida_xref
-import ida_funcs
 import idc
+import idautils
 import idaapi
-import json
-import ida_kernwin
-import ida_ua
-import ida_fixup
-import ida_name
 
 def get_dt_size(dtype):
-	if dtype == idc.dt_byte:
+	if dtype == ida_ua.dt_byte:
 		return 1
-	elif dtype == idc.dt_word:
+	elif dtype == ida_ua.dt_word:
 		return 2
-	elif dtype == idc.dt_dword:
+	elif dtype == ida_ua.dt_dword:
 		return 4
-	elif dtype == idc.dt_float:
+	elif dtype == ida_ua.dt_float:
 		return 4
-	elif dtype == idc.dt_double:
+	elif dtype == ida_ua.dt_double:
 		return 8
 	else:
 		print("Unknown type size (%d)" % dtype)
 		return -1
 
 def print_wildcards(count):
-	i = 0
-	string = ""
-	for i in xrange(count):
-		string = string + "? "
-
-	return string
+	return "? " * count
 
 def is_good_sig(sig):
 	count = 0
@@ -59,18 +47,18 @@ def makesig():
 
 	addr = funcstart
 	while addr != idc.BADADDR:
-		info = idautils.DecodeInstruction(addr)
-		if info is None:
+
+		info = ida_ua.insn_t()
+		if not ida_ua.decode_insn(info, addr):
 			return None
 
 		done = 0
-		if len(info.ops) == 1:
-			if info.ops[0].type == ida_ua.o_near or info.ops[0].type == ida_ua.o_far:
-				if hex(idc.get_wide_byte(addr)) == 0x0F: 	# Two-byte instruction
-					sig = sig + ("0F %02X " % idc.get_wide_byte(addr + 1)) + print_wildcards(get_dt_size(info.ops[0].dtype))
-				else:
-					sig = sig + ("%02X " % idc.get_wide_byte(addr)) + print_wildcards(get_dt_size(info.ops[0].dtype))
-				done = 1
+		if info.Op1.type == ida_ua.o_near or info.Op1.type == ida_ua.o_far:
+			if (idc.get_wide_byte(addr)) == 0x0F: 	# Two-byte instruction
+				sig = sig + ("0F %02X " % idc.get_wide_byte(addr + 1)) + print_wildcards(get_dt_size(info.Op1.dtype))
+			else:
+				sig = sig + ("%02X " % idc.get_wide_byte(addr)) + print_wildcards(get_dt_size(info.Op1.dtype))
+			done = 1
 
 		if not done: 	# Unknown, just wildcard addresses
 			i = 0
